70c8f03c81b2156f9649b049a6275d726beeca7a
mbashila <mbashila@gmail.com>
2024-07-28 04:01:05 +0200

    Connect to MongoDB

--- client/src/componets/Header.jsx
+++ client/src/componets/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <>
       <div className="bg-slate-200">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-          <Link to="/Home">
+          <Link to="/">
             <h1 className="font-bold">SmartMedCare</h1>
           </Link>
           <ul className="flex gap-4">
-            <Link to="/Home">
+            <Link to="/">
               <li>Home</li>
             </Link>
             <Link to="/About">
